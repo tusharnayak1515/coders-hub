c02d1266ba229b8b3d60c1b7c5499336ec3f1be2
Tushar Ranjan Nayak <tusharnayak1515@gmail.com>
2022-09-06 17:46:12 +0530

    final

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { shallowEqual, useSelector } from 'react-redux';
 
@@ -15,6 +16,11 @@ const NotFound = () => {
 
   return (
     <div className={`${styles.notFoundPage} ${theme === "light" ? styles.light_notFoundPage : styles.dark_notFoundPage}`}>
+        <Head>
+            <title>Page Not Found</title>
+            <meta name="keywords" content="next, next.js, coders-hub, blogs" />
+        </Head>
+
         <div className={styles.notFound_div}>
            <h1>404</h1>
            <h2>Page Not Found</h2>
